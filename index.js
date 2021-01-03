0db5d0579c23a0d45e20beea398734ea3414c477
manwlispro@gmail.com <manwlispro@gmail.com>
2021-01-03 13:21:01 +0200

    fix the toggle on resize next

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const nav = document.querySelector(".nav-links");
 
 burger.addEventListener('click', ()=>{
     nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
+
+nav.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
 });
